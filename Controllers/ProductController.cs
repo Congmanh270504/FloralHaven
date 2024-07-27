31f77e28ed2732f2d5ae7c3a25928558b7c28f39
LazyNora <lazynora@outlook.com>
2024-07-27 11:55:25 +0700

    Product List

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using FloralHaven.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace FloralHaven.Controllers
@@ -6,12 +7,49 @@ namespace FloralHaven.Controllers
 	public class ProductController : Controller
 	{
 		FloralHavenDataContext _db = FloralHavenDBContextConfig.GetFloralHavenDataContext();
+		string _imgPrefix = "https://congmanh270504.github.io/Db-FloralHaven/";
 
 		// GET: Product
 		[HttpGet]
 		public ActionResult Index()
 		{
-			return View();
+			int page = Request.ContentLength == 0 ? 0 : int.Parse(Request.QueryString["page"]);
+			ProductsViewModel products = new ProductsViewModel();
+			foreach (var product in _db.PRODUCTs.Skip(page * 20).Take(20))
+			{
+				string MainImage = _imgPrefix + product.handle + "/";
+				var productImage = _db.IMAGEs.FirstOrDefault(image => image.productid == product.id);
+				if (productImage != null)
+				{
+					MainImage += productImage.path;
+				}
+				string CategoryName = _db.CATEGORies.FirstOrDefault(category => category.id == product.categoryid).name;
+				products.List.Add(new ProductsViewModel_Product(product.id, product.title, product.handle, product.instock, product.price, product.sellprice, MainImage, product.categoryid, CategoryName));
+			}
+			return View(products);
+		}
+
+		// URL
+		[HttpGet]
+		[Route("Product/{handle}")]
+		public ActionResult Product(string handle)
+		{
+			var product = _db.PRODUCTs.FirstOrDefault(p => p.handle == handle);
+			if (product == null)
+			{
+				return RedirectToAction("Index");
+			}
+			string MainImage = _imgPrefix + product.handle + "/";
+			var productImage = _db.IMAGEs.FirstOrDefault(image => image.productid == product.id);
+			if (productImage != null)
+			{
+				MainImage += productImage.path;
+			}
+
+			string CategoryName = _db.CATEGORies.FirstOrDefault(category => category.id == product
+			.categoryid).name;
+			ProductsViewModel_Product productViewModel = new ProductsViewModel_Product(product.id, product.title, product.handle, product.instock.GetValueOrDefault(), product.price, product.sellprice.GetValueOrDefault(), MainImage, product.categoryid, CategoryName);
+			return View(productViewModel);
 		}
 	}
 }
---
+++ Models/ProductsViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace FloralHaven.Models
+{
+	public class ProductsViewModel
+	{
+		public List<ProductsViewModel_Product> List { get; set; }
+		public ProductsViewModel()
+		{
+			List = new List<ProductsViewModel_Product>();
+		}
+	}
+
+	public class ProductsViewModel_Product
+	{
+		public int ProductID { get; set; }
+		public string Name { get; set; }
+		public string Handle { get; set; }
+		public int? Stock { get; set; }
+		public decimal Price { get; set; }
+		public decimal? SalePrice { get; set; }
+		public string MainImage { get; set; }
+		public string CategoryID { get; set; }
+		public string CategoryName { get; set; }
+
+		public ProductsViewModel_Product(int productID, string name, string handle, int? stock, decimal price, decimal? salePrice, string mainImage, string categoryID, string categoryName)
+		{
+			ProductID = productID;
+			Name = name;
+			Handle = handle;
+			Stock = stock;
+			Price = price;
+			SalePrice = salePrice;
+			MainImage = mainImage;
+			CategoryID = categoryID;
+			CategoryName = categoryName;
+		}
+	}
+}
